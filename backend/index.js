27b80c36d6e7d177adefe6fef6df5b5e418c269b
Aswin N A <aswin50@gamil.com>
2025-08-24 23:16:50 +0530

    database connected

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,17 @@
-console.log("welcome");
+import express from 'express'
+import ConnectDB from './Config/ConnectDB.js';
+import dotenv from 'dotenv'
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.json({ message: "List of todos" });
+});
+
+const PORT = process.env.PORT || 5000;
+ConnectDB()
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
